dcac1aed728df4260f6d6fd12b5ddabe70f32180
ikram uygur <123154240+marky731@users.noreply.github.com>
2025-02-03 16:02:14 +0300

    init error

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DatabaseService;
+using WebApplication1.Dtos;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserService _dapper;
+
+        public UserController(IConfiguration config)
+        {
+            _dapper = new UserService(config);
+        }
+
+        [HttpGet("")]
+        public DateTime TestConnection()
+        {
+            return _dapper.LoadDataSingle<DateTime>("SELECT NOW()");
+        }
+
+
+        [HttpGet("")]
+        public IEnumerable<User> GetUsers()
+        {
+            string sql = @"
+            SELECT *
+            FROM TutorialAppSchema.Users";
+            IEnumerable<User> users = _dapper.LoadData<User>(sql);
+            return users;
+        }
+
+        [HttpGet(" ")]
+        public User GetSingleUser(int userId)
+        {
+            string sql = @"
+            SELECT *
+            FROM TutorialAppSchema.Users
+                WHERE id = " + userId.ToString(); //"7"
+            User user = _dapper.LoadDataSingle<User>(sql);
+            return user;
+        }
+
+        [HttpPut("  ")]
+        public IActionResult EditUser(User user)
+        {
+            string sql = @"
+        UPDATE TutorialAppSchema.Users
+            SET FirstName = '" + user.FirstName +
+                         "', LastName = '" + user.LastName +
+                         "', Email = '" + user.Email +
+                         "', Gender = '" + user.Gender +
+                         "', Active = '" + user.Active +
+                         "' WHERE id = " + user.Id;
+
+            Console.WriteLine(sql);
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Update User");
+        }
+
+
+        [HttpPost("   ")]
+        public IActionResult AddUser(UserToAddDto user)
+        {
+            string sql = @"
+            INSERT INTO TutorialAppSchema.Users(
+                FirstName,
+                LastName,
+                Email,
+                Gender,
+                Active
+            ) VALUES (" +
+                         "'" + user.FirstName +
+                         "', '" + user.LastName +
+                         "', '" + user.Email +
+                         "', '" + user.Gender +
+                         "', '" + user.Active +
+                         "')";
+
+            Console.WriteLine(sql);
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Add User");
+        }
+
+        [HttpDelete("    ")]
+        public IActionResult DeleteUser(int userId)
+        {
+            string sql = @"
+            DELETE FROM TutorialAppSchema.Users 
+                WHERE id = " + userId.ToString();
+
+            Console.WriteLine(sql);
+
+            if (_dapper.ExecuteSql(sql))
+            {
+                return Ok();
+            }
+
+            throw new Exception("Failed to Delete User");
+        }
+    }
+}
---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,12 @@
+namespace WebApplication1.Models
+{
+    public  class User
+    {
+        public int Id { get; set; } // veri tabanini duzenledim
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Gender { get; set; }
+        public bool Active { get; set; }
+    }
+}
